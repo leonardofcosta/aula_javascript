0a927dabe536461aa7fdfadd5c1204ac9a185c43
Leonardo F Costa <leonardofcosta@gmail.com>
2020-09-13 00:36:48 -0300

    Adicionado pasta js e arquivo javascript para estudos iniciais

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Fulano Ciclano"
+var idade = 40
+
+var frase = "Japão é o melhor time do mundo"
+
+console.log("nome: " + nome + " idade: " + idade)
+
+console.log(frase.replace("Japão", "Brasil"))
+
+console.log(frase.toUpperCase())
