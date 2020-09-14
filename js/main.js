a701ed94954dfc7be095d9641f441c436c211902
Leonardo F Costa <leonardofcosta@gmail.com>
2020-09-14 00:37:25 -0300

    Estudo sobre vetores

--- js/main.js
+++ js/main.js
@@ -8,3 +8,35 @@ console.log("nome: " + nome + " idade: " + idade)
 console.log(frase.replace("Japão", "Brasil"))
 
 console.log(frase.toUpperCase())
+
+
+/* Array */
+
+var lista = ["maçã", "pêra", "laranja"]
+
+// imprime todo array
+console.log(lista)
+
+// imprime elemento que esta na posição um
+console.log(lista[1])
+
+// adicionar novo item a lista
+lista.push("uva")
+console.log(lista)
+
+// retirar elemento do array
+lista.pop()
+console.log(lista)
+
+// ver tamanho da lista
+console.log(lista.length)
+
+// inverter ordem dos elementos
+lista.reverse()
+console.log(lista)
+
+// converte array para string
+console.log(lista.toString());
+
+// converte para string e adiciona um caracter, a escolher, como um separador
+console.log(lista.join(' - '))
